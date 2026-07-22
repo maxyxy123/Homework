d53a77eed5bf97ac9d317fcc29da8ce85411f82c
maxyxy123 <email@gmail.com>
2026-07-22 16:44:56 +0700

    done

---
+++ module2-session1/BTTH/bai2.js
@@ -0,0 +1,9 @@
+const myUniversity = "FPT University";
+let studentName = "Nguyễn Văn An";
+let studentAge = 20;
+let isProgrammingFun = true;
+
+console.log("Trường đại học:", myUniversity);
+console.log("Tên sinh viên:", studentName);
+console.log("Tuổi sinh viên:", studentAge);
+console.log("Lập trình có thú vị không:", isProgrammingFun);
